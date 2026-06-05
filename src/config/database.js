10b07b0f19c6eba55a465b71dd09ecec7cc571c3
Dio Prasetyo <prasetyodio04@gmail.com>
2026-06-05 11:44:09 +0700

    Update Readme

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const dns = require("dns");
+// const dns = require("dns");
 
 // Force Google Public DNS to resolve MongoDB Atlas SRV records
 // Fixes: queryTxt ETIMEOUT cluster0.xxx.mongodb.net
